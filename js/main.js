51233cdbd55149c996cbdc5da387665f8518137f
Rachael <62398601+rachaelthemes@users.noreply.github.com>
2021-02-13 15:40:37 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,105 @@
-
+// NPF IMAGES READJUSTMENT | @glenthemes
+$(document).ready(function() {
+    // wrap each row
+    $(".npf_row").each(function() {
+        $(this).wrap("<div class='npf_row_set'>");
+    });
+    // make each row (and its items) even
+    $(".tmblr-full").each(function() {
+        if ($(this).closest(".npf_col").length) {
+            $(this).unwrap()
+        }
+        var imgheight = $(this).height();
+        if (imgheight < $(this).parents(".npf_row").height()) {
+            $(this).parents(".npf_row").height(imgheight)
+        }
+        if (!$(this).closest(".npf_row").length) {
+            $(this).wrap("<div class='npf_row_parent'>");
+        };
+    });
+    $(".npf_row_parent").each(function() {
+        if ($(this).siblings(".npf_row_parent").length) {
+            $(this).addClass("bROTHER");
+        }
+        if (!$(this).closest(".npf_row_set").length) {
+            $(this).wrap("<div class='npf_row_set'>");
+        }
+        if (!$(this).siblings().length) {
+            if ($(this).closest(".npf_row_set").length) {
+                $(this).unwrap();
+            }
+        }
+        if (!$(this).hasClass("bROTHER")) {
+            $(this).wrap("<div class='npf_inst'>");
+        }
+    });
+    var divs = $(".bROTHER");
+    for (var i = 0; i < divs.length;) {
+        i += divs.eq(i).nextUntil(':not(.bROTHER)').andSelf().wrapAll('<div class="npf_inst">').length;
+    }
+    $(".npf_row_set").each(function() {
+        if (!$(this).closest(".npf_inst").length) {
+            if (!$(this).parent("blockquote").length) {
+                $(this).parent().children(".npf_row_set").wrapAll("<div class='npf_inst'>");
+            }
+        };
+    });
+    $(".tmblr-full").click(function() {
+        var img = $(this).find("img"),
+            imgsrc = img.attr("src");
+        Tumblr.Lightbox.init([{
+            low_res: imgsrc,
+            high_res: imgsrc
+        }]);
+        $("body").toggleClass("tumblr_lightbox_active");
+        return false;
+    });
+    $("blockquote").each(function() {
+        $(this).children(".npf_row_set").wrapAll("<div class='npf_inst'>");
+    });
+    $("*").filter('[post-type="text"]').addClass('textpost');
+    $(".textpost").each(function() {
+        if ($(this).find("blockquote").next(".npf_row_set").length) {
+            $(this).find("blockquote").siblings(".npf_row_set").wrapAll('<div class="npf_inst">')
+        }
+    });
+    $(".npf_inst img").each(function() {
+        var ogwidth = $(this).attr("data-orig-width"),
+            imgurl = $(this).attr("src").split("_").pop(),
+            imgnum = parseInt(imgurl);
+        if (imgnum < ogwidth) {
+            var newsrc = $(this).attr("src").replace("_" + imgnum + ".", "_" + 1280 + ".");
+            $(this).attr("src", newsrc);
+        }
+    });
+    $(".npf_inst").each(function() {
+        var checkpara = $(this).next("p");
+        if (checkpara.length) {
+            if (checkpara.children("br").length == 1) {
+                checkpara.remove();
+            }
+        }
+    });
+    $(".textpost p").each(function() {
+        if ($(this).text().length == 0) {
+            if ($(this).children().length == 0) {
+                if ($(this).prev().length == 0) {
+                    if ($(this).next(".npf_inst")) {
+                        $(this).remove();
+                    }
+                }
+            }
+        }
+    });
+    $(".tmblr-full img").each(function() {
+        $(this).attr("genheight", $(this).height());
+    });
+    $(".npf_row_set").each(function() {
+        var que = $(this).find(".tmblr-full img").map(function() {
+            return $(this).attr("genheight");
+        }).get();
+        var shortest = Math.min.apply(Math, que);
+        $(this).find(".npf_row").height(shortest);
+        $(this).find(".tmblr-full").height(shortest)
+    });
+});
