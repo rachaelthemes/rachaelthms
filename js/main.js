275073669cdcec8abe806f8b3c04d976cf37dbef
Rachael <62398601+rachaelthemes@users.noreply.github.com>
2021-02-13 20:32:44 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,42 +3,9 @@
 if (location.href == 'https://rachaelthemes.com/ask') location.replace('https://rachaelthemes.com/help');
 if (location.href == 'https://rachaelthemes.com/submit') location.replace('https://rachaelthemes.com/help');
 
-// dark mode cookies (using js.cookie)
+// mobile nav open/close trigger
 
-var dm = Cookies.get("dark");
-
-function darkOn() {
-    Cookies.set('dark', 'on');
-    $('body').addClass('dark');
-    dm = "on";
-};
-
-function darkOff() {
-    Cookies.set('dark', 'off');
-    $('body').removeClass('dark');
-    dm = "off";
-};
 jQuery(document).ready(function() {
-    if (Cookies.get("dark")) {
-        dm = Cookies.get("dark");
-        if (dm == "on") {
-            darkOn();
-        }
-    } else {
-        Cookies.set('dark', 'off', {
-            expires: 90
-        });
-    };
-    $('#dark-mode').click(function() {
-        if (dm == "off") {
-            darkOn();
-        } else if (dm == "on") {
-            darkOff();
-        }
-    });
-    
-    // mobile nav open/close trigger
-    
     $('#mobile-open').click(function() {
         $('#mobile-nav').fadeIn(300);
     });
@@ -46,3 +13,22 @@ jQuery(document).ready(function() {
         $('#mobile-nav').fadeOut(300);
     });
 });
+
+// page transitions
+
+$('#screen').show();
+window.onload = function() {
+    $('#screen').fadeOut(500);
+};
+
+$(document).on("click", "a.internal", function() {
+    var redirect = $(this).attr("href");
+    if (!redirect || redirect[0] === "#") {
+        location.hash = redirect;
+        return;
+    }
+    $("#screen").fadeIn(500, function() {
+        location = redirect;
+       });
+    return false;
+}); 
